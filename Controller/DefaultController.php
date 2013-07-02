6880f73f50cf3525199e8bdcfaa4e7f294fecca1
Kévin Gomez <geek63@gmail.com>
2013-07-02 14:04:17 +0100

    Fix: get rid of the dependency to the framework bundle

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,33 +2,29 @@
 
 namespace ITN\Bundle\LogViewerBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use Tac\Tac;
 
 
-class DefaultController extends Controller
+class DefaultController extends ContainerAware
 {
-    /**
-     * @Route("/{env}")
-     * @Template()
-     */
-    public function indexAction($env)
+    public function indexAction(Request $request, $env)
     {
         $logs_dir = $this->container->getParameter('kernel.logs_dir');
         $logfile = $logs_dir . DIRECTORY_SEPARATOR . $env . '.log';
 
         if (!file_exists($logfile)) {
-            throw $this->createNotFoundException(sprintf('log file not found %s', $logfile));
+            throw new NotFoundHttpException(sprintf('log file not found %s', $logfile));
         }
 
-        return array(
+        return $this->container->get('templating')->renderResponse('ITNLogViewerBundle:Default:index.html.twig', array(
             'tac'   => new Tac($logfile),
             'env'   => $env,
             'file'  => $logfile,
-            'lines' => (int) $this->getRequest()->query->get('lines', 1000),
-        );
+            'lines' => (int) $request->query->get('lines', 1000),
+        ));
     }
 }
